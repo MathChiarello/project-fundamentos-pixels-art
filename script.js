c3f9086b12d9fc8b0dc227474226b0c735ae4be8
Matheus Chiarello <chiarellomatheus@gmail.com>
2023-07-19 01:32:05 -0300

    Desenvolvido os requisitos do 1 ao 5 já testados com o lint e cypress

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-//Capturando Elementos
+// Capturando Elementos
 
-const colorPalette = document.querySelector('#color-palette')
-const pixelBoard = document.querySelector('.pixel-board')
+const colorPalette = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector('#pixel-board');
 
 // Criando constantes
 
-const colors = ['violet','green','red','blue'];
+const colors = ['violet', 'green', 'red', 'blue'];
 const pixelsLenght = 5;
+
+// Função onClick
+
+function getColorSelected(event) {
+  let selectedElement = document.querySelector('.selected');
+  const elemento = event.target;
+
+  if (selectedElement === null) {
+    selectedElement = elemento;
+  }
+
+  selectedElement.classList.remove('selected');
+  elemento.classList.add('selected');
+}
+
+function setColorSelected(event) {
+  const selectedItem = document.querySelector('.selected');
+  const element = event.target;
+  element.style.backgroundColor = selectedItem.style.backgroundColor;
+}
 
 // Paleta de cores
 
 for (let index = 0; index < colors.length; index += 1) {
-  colorItens = document.createElement('div');
+  const colorItens = document.createElement('div');
   colorPalette.appendChild(colorItens);
   colorItens.className = 'color';
   colorItens.style.backgroundColor = colors[index];
-  colorItens.setAttribute('onclick', 'teste()');
+  colorItens.addEventListener('click', getColorSelected);
 }
-
 
 // Pixel Board
 
 for (let indexPixelColumn = 0; indexPixelColumn < pixelsLenght; indexPixelColumn += 1) {
   for (let indexPixelRow = 0; indexPixelRow < pixelsLenght; indexPixelRow += 1) {
-    pixel = document.createElement('div');
-    pixelBoard.appendChild(pixel)  
+    const pixel = document.createElement('div');
+    pixelBoard.appendChild(pixel);
     pixel.className = 'pixel';
-  }    
+    pixel.addEventListener('click', setColorSelected);
+  }
 }
 
-// Função onClick
-
-function teste() {
-  console.log(addEventListener("click",className = 'selected'));
+function reset() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'rgb(255, 255, 255)';
+  }
 }
 
-
-console.log(colorPalette);
+const button = document.createElement('button');
+document.querySelector('body').insertBefore(button, document.querySelector('#pixel-board'));
+button.id = 'clear-board';
+button.textContent = 'Limpar';
+button.addEventListener('click', reset);
